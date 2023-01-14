dab567e99c3318ba5dbd79228737ccaaf4af50c1
zaynhakim <zaynhakim@gmail.com>
2023-01-14 23:14:35 +0700

    reverted

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,5 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zaynhakim.github.io",
-  base: "/astroblog",
   integrations: [preact(), tailwind(), mdx()],
 });
